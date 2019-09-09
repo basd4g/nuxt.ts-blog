ff3c3111ad3d7206b391d400f5fad3346dfe9db7
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-09 22:20:00 +0900

    Add dinamic routing

---
+++ components/Article.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{articleId}}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Article extends Vue {
+    @Prop({ type: String, required: true })
+    articleId: String
+}
+</script>
---
+++ pages/article/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Article v-bind:articleId="$route.params.id"></Article>
+    </div>
+   
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Article from '~/components/Article.vue'
+
+@Component({
+  components: {
+    Article
+  }
+})
+export default class Index extends Vue {
+}
+</script>
+ 
